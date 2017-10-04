254035b05c5b51e46c6def713dce9339530a634f
Mudafar <mudafar@ula.ve>
2017-10-04 12:24:00 -0400

    Visitors: Added iframe method and route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users, controllers: { registrations: 'registrations' }
   resources :users
+  resources :visitors, only: [:index]
+
+  get 'iframe', to: 'visitors#iframe'
 end
